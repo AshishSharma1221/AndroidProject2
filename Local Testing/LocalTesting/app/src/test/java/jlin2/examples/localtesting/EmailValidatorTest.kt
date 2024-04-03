35a8a0e1dcd7c998631f55e1098b2cb58517735d
impawandeepsg <83604543+impawandeepsg@users.noreply.github.com>
2024-04-03 00:00:05 -0400

    Implemented new tests; closes #1 , #3

---
+++ Local Testing/LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -0,0 +1,18 @@
+package jlin2.examples.localtesting
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class EmailValidatorTest {
+
+    @Test
+    fun testValidEmail() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun testInvalidEmail() {
+        assertFalse(EmailValidator.isValidEmail("123@abc"))
+    }
+}
