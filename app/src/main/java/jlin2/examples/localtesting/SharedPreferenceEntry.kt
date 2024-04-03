6263acb3fb9b34d55aba936c71eaf10fcefca94c
Pawandeep Singh <83604543+impawandeepsg@users.noreply.github.com>
2024-04-02 21:19:44 -0400

    Delete app directory

--- app/src/main/java/jlin2/examples/localtesting/SharedPreferenceEntry.kt
+++
@@ -1,8 +0,0 @@
-package jlin2.examples.localtesting
-
-import java.util.Calendar
-
-/**
- * Model class containing personal information that will be saved to SharedPreferences.
- */
-class SharedPreferenceEntry(val name: String, val dateOfBirth: Calendar, val email: String)
